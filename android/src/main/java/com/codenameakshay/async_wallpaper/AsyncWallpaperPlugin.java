2b5f57d788befbad32086f2e6c680fbdc8bd7535
thanhnguyen <thanh.nguyen@go2joy.vn>
2025-09-13 15:50:00 +0700

    Update

--- android/src/main/java/com/codenameakshay/async_wallpaper/AsyncWallpaperPlugin.java
+++ android/src/main/java/com/codenameakshay/async_wallpaper/AsyncWallpaperPlugin.java
@@ -308,6 +308,148 @@ public class AsyncWallpaperPlugin extends Application implements FlutterPlugin, 
     }
 }
 
+// class SetWallPaperTask extends AsyncTask<Pair<Bitmap, String>, Boolean, Boolean> {
+
+//     private final Context mContext;
+
+//     public SetWallPaperTask(final Context context) {
+//         mContext = context;
+//     }
+
+//     @Override
+//     protected final Boolean doInBackground(Pair<Bitmap, String>... pairs) {
+//         switch (pairs[0].second) {
+//             case "1": {
+//                 WallpaperManager wallpaperManager = WallpaperManager.getInstance(mContext);
+//                 try {
+//                     int WITH_OTHER_APP_CODE = 733;
+//                     Uri tempUri = getImageUri(mContext, pairs[0].first);
+//                     Log.i("Arguments ", "configureFlutterEngine: " + "Saved image to storage");
+//                     File finalFile = new File(getRealPathFromURI(tempUri));
+//                     Uri contentURI = getImageContentUri(mContext, finalFile.getAbsolutePath());
+//                     Log.i("Arguments ", "configureFlutterEngine: " + "Opening crop intent");
+//                     Intent setWall = new Intent(Intent.ACTION_ATTACH_DATA);
+//                     setWall.setDataAndType(contentURI, "image/*");
+//                     setWall.putExtra("mimeType", "image/*");
+//                     setWall.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+//                     setWall.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                     Intent chooser = Intent.createChooser(setWall, "Apply with external app");
+//                     chooser.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                     mContext.startActivity(
+//                             chooser,
+//                             new Bundle(WITH_OTHER_APP_CODE));
+//                 } catch (Exception ex) {
+//                     try {
+//                         wallpaperManager.setBitmap(pairs[0].first);
+//                     } catch (IOException e) {
+//                         e.printStackTrace();
+//                     }
+//                     ex.printStackTrace();
+//                     return false;
+//                 }
+//             }
+//             case "2": {
+//                 WallpaperManager wallpaperManager = WallpaperManager.getInstance(mContext);
+//                 try {
+//                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+//                         wallpaperManager.setBitmap(pairs[0].first, null, true, WallpaperManager.FLAG_LOCK);
+//                     }
+//                 } catch (IOException ex) {
+//                     ex.printStackTrace();
+//                     return false;
+//                 }
+//                 break;
+//             }
+//             case "3": {
+//                 WallpaperManager wallpaperManager = WallpaperManager.getInstance(mContext);
+//                 try {
+//                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+//                         wallpaperManager.setBitmap(pairs[0].first, null, true, WallpaperManager.FLAG_SYSTEM);
+//                     }
+//                 } catch (IOException ex) {
+//                     ex.printStackTrace();
+//                     return false;
+//                 }
+//                 break;
+//             }
+//             case "4": {
+//                 WallpaperManager wallpaperManager = WallpaperManager.getInstance(mContext);
+//                 try {
+//                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+//                         wallpaperManager.setBitmap(pairs[0].first, null, true,
+//                                 WallpaperManager.FLAG_LOCK | WallpaperManager.FLAG_SYSTEM);
+//                     }
+//                 } catch (IOException ex) {
+//                     ex.printStackTrace();
+//                     return false;
+//                 }
+//                 break;
+//             }
+//         }
+//         return true;
+//     }
+
+//     @Override
+//     protected void onPostExecute(Boolean aBoolean) {
+//         myMethod(aBoolean);
+//     }
+
+//     private void myMethod(Boolean result) {
+//         AsyncWallpaperPlugin.res.success(result);
+//     }
+
+//     public static Uri getImageContentUri(Context context, String absPath) {
+
+//         Cursor cursor = context.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+//                 new String[] { MediaStore.Images.Media._ID }, MediaStore.Images.Media.DATA + "=? ",
+//                 new String[] { absPath }, null);
+
+//         if (cursor != null && cursor.moveToFirst()) {
+//             int id = cursor.getInt(cursor.getColumnIndex(MediaStore.MediaColumns._ID));
+//             return Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, Integer.toString(id));
+
+//         } else if (!absPath.isEmpty()) {
+//             ContentValues values = new ContentValues();
+//             values.put(MediaStore.Images.Media.DATA, absPath);
+//             return context.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+//         } else {
+//             return null;
+//         }
+//     }
+
+//     public Uri getImageUri(Context inContext, Bitmap inImage) {
+//         ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+//         inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+//         fixMediaDir();
+//         String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+//         return Uri.parse(path);
+//     }
+
+//     public String getRealPathFromURI(Uri uri) {
+//         Cursor cursor = mContext.getContentResolver().query(uri, null, null, null, null);
+//         cursor.moveToFirst();
+//         int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+//         return cursor.getString(idx);
+//     }
+
+//     void fixMediaDir() {
+//         File sdcard = Environment.getExternalStorageDirectory();
+//         if (sdcard != null) {
+//             File mediaDir = new File(sdcard, "DCIM/Camera");
+//             if (!mediaDir.exists()) {
+//                 mediaDir.mkdirs();
+//             }
+//         }
+
+//         if (sdcard != null) {
+//             File mediaDir = new File(sdcard, "Pictures");
+//             if (!mediaDir.exists()) {
+//                 mediaDir.mkdirs();
+//             }
+//         }
+//     }
+// }
+
 class SetWallPaperTask extends AsyncTask<Pair<Bitmap, String>, Boolean, Boolean> {
 
     private final Context mContext;
@@ -318,134 +460,56 @@ class SetWallPaperTask extends AsyncTask<Pair<Bitmap, String>, Boolean, Boolean>
 
     @Override
     protected final Boolean doInBackground(Pair<Bitmap, String>... pairs) {
-        switch (pairs[0].second) {
-            case "1": {
-                WallpaperManager wallpaperManager = WallpaperManager.getInstance(mContext);
-                try {
-                    int WITH_OTHER_APP_CODE = 733;
-                    Uri tempUri = getImageUri(mContext, pairs[0].first);
-                    Log.i("Arguments ", "configureFlutterEngine: " + "Saved image to storage");
-                    File finalFile = new File(getRealPathFromURI(tempUri));
-                    Uri contentURI = getImageContentUri(mContext, finalFile.getAbsolutePath());
-                    Log.i("Arguments ", "configureFlutterEngine: " + "Opening crop intent");
-                    Intent setWall = new Intent(Intent.ACTION_ATTACH_DATA);
-                    setWall.setDataAndType(contentURI, "image/*");
-                    setWall.putExtra("mimeType", "image/*");
-                    setWall.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                    setWall.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    Intent chooser = Intent.createChooser(setWall, "Apply with external app");
-                    chooser.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    mContext.startActivity(
-                            chooser,
-                            new Bundle(WITH_OTHER_APP_CODE));
-                } catch (Exception ex) {
-                    try {
-                        wallpaperManager.setBitmap(pairs[0].first);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    ex.printStackTrace();
-                    return false;
+        WallpaperManager wallpaperManager = WallpaperManager.getInstance(mContext);
+
+        try {
+            switch (pairs[0].second) {
+                case "1": {
+                    // Cũ: mở chooser crop bằng Intent -> reset app
+                    // Mới: set trực tiếp (áp dụng mặc định)
+                    wallpaperManager.setBitmap(pairs[0].first);
+                    break;
                 }
-            }
-            case "2": {
-                WallpaperManager wallpaperManager = WallpaperManager.getInstance(mContext);
-                try {
+                case "2": {
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                         wallpaperManager.setBitmap(pairs[0].first, null, true, WallpaperManager.FLAG_LOCK);
+                    } else {
+                        wallpaperManager.setBitmap(pairs[0].first);
                     }
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                    return false;
+                    break;
                 }
-                break;
-            }
-            case "3": {
-                WallpaperManager wallpaperManager = WallpaperManager.getInstance(mContext);
-                try {
+                case "3": {
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                         wallpaperManager.setBitmap(pairs[0].first, null, true, WallpaperManager.FLAG_SYSTEM);
+                    } else {
+                        wallpaperManager.setBitmap(pairs[0].first);
                     }
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                    return false;
+                    break;
                 }
-                break;
-            }
-            case "4": {
-                WallpaperManager wallpaperManager = WallpaperManager.getInstance(mContext);
-                try {
+                case "4": {
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                        wallpaperManager.setBitmap(pairs[0].first, null, true,
-                                WallpaperManager.FLAG_LOCK | WallpaperManager.FLAG_SYSTEM);
+                        wallpaperManager.setBitmap(
+                                pairs[0].first,
+                                null,
+                                true,
+                                WallpaperManager.FLAG_LOCK | WallpaperManager.FLAG_SYSTEM
+                        );
+                    } else {
+                        wallpaperManager.setBitmap(pairs[0].first);
                     }
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                    return false;
+                    break;
                 }
-                break;
             }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return false;
         }
+
         return true;
     }
 
     @Override
     protected void onPostExecute(Boolean aBoolean) {
-        myMethod(aBoolean);
-    }
-
-    private void myMethod(Boolean result) {
-        AsyncWallpaperPlugin.res.success(result);
-    }
-
-    public static Uri getImageContentUri(Context context, String absPath) {
-
-        Cursor cursor = context.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                new String[] { MediaStore.Images.Media._ID }, MediaStore.Images.Media.DATA + "=? ",
-                new String[] { absPath }, null);
-
-        if (cursor != null && cursor.moveToFirst()) {
-            int id = cursor.getInt(cursor.getColumnIndex(MediaStore.MediaColumns._ID));
-            return Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, Integer.toString(id));
-
-        } else if (!absPath.isEmpty()) {
-            ContentValues values = new ContentValues();
-            values.put(MediaStore.Images.Media.DATA, absPath);
-            return context.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-        } else {
-            return null;
-        }
-    }
-
-    public Uri getImageUri(Context inContext, Bitmap inImage) {
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-        fixMediaDir();
-        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
-        return Uri.parse(path);
-    }
-
-    public String getRealPathFromURI(Uri uri) {
-        Cursor cursor = mContext.getContentResolver().query(uri, null, null, null, null);
-        cursor.moveToFirst();
-        int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
-        return cursor.getString(idx);
-    }
-
-    void fixMediaDir() {
-        File sdcard = Environment.getExternalStorageDirectory();
-        if (sdcard != null) {
-            File mediaDir = new File(sdcard, "DCIM/Camera");
-            if (!mediaDir.exists()) {
-                mediaDir.mkdirs();
-            }
-        }
-
-        if (sdcard != null) {
-            File mediaDir = new File(sdcard, "Pictures");
-            if (!mediaDir.exists()) {
-                mediaDir.mkdirs();
-            }
-        }
+        AsyncWallpaperPlugin.res.success(aBoolean);
     }
 }
