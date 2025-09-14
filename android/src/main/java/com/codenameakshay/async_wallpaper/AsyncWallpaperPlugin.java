ca131551fece9a00a414f11cf16e41474a9558f2
thanhnguyen <thanh.nguyen@go2joy.vn>
2025-09-14 10:51:54 +0700

    Update

--- android/src/main/java/com/codenameakshay/async_wallpaper/AsyncWallpaperPlugin.java
+++ android/src/main/java/com/codenameakshay/async_wallpaper/AsyncWallpaperPlugin.java
@@ -3,11 +3,6 @@ package com.codenameakshay.async_wallpaper;
 import android.app.Activity;
 import android.app.Application;
 import android.app.WallpaperManager;
-import android.graphics.BitmapFactory;
-import android.content.ContentValues;
-import android.content.Context;
-import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
@@ -15,7 +10,10 @@ import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.MediaStore;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.util.Log;
 import android.util.Pair;
 
@@ -37,13 +35,11 @@ import io.flutter.embedding.engine.plugins.activity.ActivityAware;
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
-import io.flutter.plugin.common.MethodChannel.Result;
 
 /**
  * AsyncWallpaperPlugin
  */
-public class AsyncWallpaperPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
+public class AsyncWallpaperPlugin extends Application implements FlutterPlugin, MethodChannel.MethodCallHandler, ActivityAware {
     private MethodChannel channel;
     public static Context context;
     private Activity activity;
@@ -55,42 +51,54 @@ public class AsyncWallpaperPlugin implements FlutterPlugin, MethodCallHandler, A
     private Target target = new Target() {
         @Override
         public void onBitmapLoaded(Bitmap resource, Picasso.LoadedFrom from) {
-            Log.i("Arguments ", "Image Downloaded (default)");
             SetWallPaperTask setWallPaperTask = new SetWallPaperTask(context);
-            setWallPaperTask.execute(new Pair<>(resource, "1"));
+            setWallPaperTask.execute(new Pair(resource, "1"));
         }
-        @Override public void onBitmapFailed(Exception e, Drawable errorDrawable) {}
-        @Override public void onPrepareLoad(Drawable placeHolderDrawable) {}
+
+        @Override
+        public void onBitmapFailed(Exception e, Drawable errorDrawable) { }
+
+        @Override
+        public void onPrepareLoad(Drawable placeHolderDrawable) { }
     };
     private Target target1 = new Target() {
         @Override
         public void onBitmapLoaded(Bitmap resource, Picasso.LoadedFrom from) {
-            Log.i("Arguments ", "Image Downloaded (lock)");
             SetWallPaperTask setWallPaperTask = new SetWallPaperTask(context);
-            setWallPaperTask.execute(new Pair<>(resource, "2"));
+            setWallPaperTask.execute(new Pair(resource, "2"));
         }
-        @Override public void onBitmapFailed(Exception e, Drawable errorDrawable) {}
-        @Override public void onPrepareLoad(Drawable placeHolderDrawable) {}
+
+        @Override
+        public void onBitmapFailed(Exception e, Drawable errorDrawable) { }
+
+        @Override
+        public void onPrepareLoad(Drawable placeHolderDrawable) { }
     };
     private Target target2 = new Target() {
         @Override
         public void onBitmapLoaded(Bitmap resource, Picasso.LoadedFrom from) {
-            Log.i("Arguments ", "Image Downloaded (home)");
             SetWallPaperTask setWallPaperTask = new SetWallPaperTask(context);
-            setWallPaperTask.execute(new Pair<>(resource, "3"));
+            setWallPaperTask.execute(new Pair(resource, "3"));
         }
-        @Override public void onBitmapFailed(Exception e, Drawable errorDrawable) {}
-        @Override public void onPrepareLoad(Drawable placeHolderDrawable) {}
+
+        @Override
+        public void onBitmapFailed(Exception e, Drawable errorDrawable) { }
+
+        @Override
+        public void onPrepareLoad(Drawable placeHolderDrawable) { }
     };
     private Target target3 = new Target() {
         @Override
         public void onBitmapLoaded(Bitmap resource, Picasso.LoadedFrom from) {
-            Log.i("Arguments ", "Image Downloaded (both)");
             SetWallPaperTask setWallPaperTask = new SetWallPaperTask(context);
-            setWallPaperTask.execute(new Pair<>(resource, "4"));
+            setWallPaperTask.execute(new Pair(resource, "4"));
         }
-        @Override public void onBitmapFailed(Exception e, Drawable errorDrawable) {}
-        @Override public void onPrepareLoad(Drawable placeHolderDrawable) {}
+
+        @Override
+        public void onBitmapFailed(Exception e, Drawable errorDrawable) { }
+
+        @Override
+        public void onPrepareLoad(Drawable placeHolderDrawable) { }
     };
 
     @Override
@@ -103,35 +111,25 @@ public class AsyncWallpaperPlugin implements FlutterPlugin, MethodCallHandler, A
     }
 
     @Override
-    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-        channel.setMethodCallHandler(null);
-    }
+    public void onDetachedFromActivity() { }
 
     @Override
-    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
-        activity = binding.getActivity();
-    }
-
-    @Override
-    public void onDetachedFromActivity() {
-        activity = null;
-    }
-
-    @Override
-    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
-        activity = binding.getActivity();
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding flutterPluginBinding) {
         if (redirectToLiveWallpaper && goToHome) {
             home();
         }
     }
 
     @Override
-    public void onDetachedFromActivityForConfigChanges() {
-        activity = null;
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding flutterPluginBinding) {
+        activity = flutterPluginBinding.getActivity();
     }
 
+    @Override
+    public void onDetachedFromActivityForConfigChanges() { }
+
     public void home() {
-        if (activity != null) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             Intent intent = new Intent(Intent.ACTION_MAIN);
             intent.addCategory(Intent.CATEGORY_HOME);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -141,82 +139,47 @@ public class AsyncWallpaperPlugin implements FlutterPlugin, MethodCallHandler, A
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
-    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+    public void onMethodCall(@NonNull MethodCall call, @NonNull MethodChannel.Result result) {
         res = result;
-        String url = call.argument("url");
-        goToHome = call.argument("goToHome") != null && (Boolean) call.argument("goToHome");
-
+        String url;
         switch (call.method) {
             case "getPlatformVersion":
-                result.success("Android " + Build.VERSION.RELEASE);
+                result.success("Android " + android.os.Build.VERSION.RELEASE);
                 break;
-
             case "set_wallpaper":
+                url = call.argument("url");
+                goToHome = call.argument("goToHome");
                 Picasso.get().load(url).into(target);
                 break;
-
             case "set_wallpaper_file":
+                url = call.argument("url");
+                goToHome = call.argument("goToHome");
                 Picasso.get().load("file://" + url).into(target);
                 break;
-
             case "set_lock_wallpaper":
+                url = call.argument("url");
+                goToHome = call.argument("goToHome");
                 Picasso.get().load(url).into(target1);
-                if (goToHome) home();
                 break;
-
             case "set_home_wallpaper":
+                url = call.argument("url");
+                goToHome = call.argument("goToHome");
                 Picasso.get().load(url).into(target2);
-                if (goToHome) home();
                 break;
-
             case "set_both_wallpaper":
+                url = call.argument("url");
+                goToHome = call.argument("goToHome");
                 Picasso.get().load(url).into(target3);
-                if (goToHome) home();
                 break;
-
-            case "set_lock_wallpaper_file":
-                Picasso.get().load("file://" + url).into(target1);
-                if (goToHome) home();
-                break;
-
-            case "set_home_wallpaper_file":
-                Picasso.get().load("file://" + url).into(target2);
-                if (goToHome) home();
-                break;
-
-            case "set_both_wallpaper_file":
-                Picasso.get().load("file://" + url).into(target3);
-                if (goToHome) home();
-                break;
-
-            case "set_video_wallpaper":
-                copyFile(new File(url), new File(activity.getFilesDir(), "file.mp4"));
-                redirectToLiveWallpaper = false;
-                VideoLiveWallpaper videoLiveWallpaper = new VideoLiveWallpaper();
-                videoLiveWallpaper.setToWallPaper(context);
-                result.success(true);
-                break;
-
-            case "open_wallpaper_chooser":
-                VideoLiveWallpaper chooser = new VideoLiveWallpaper();
-                chooser.openWallpaperChooser(context);
-                result.success(true);
-                break;
-
             default:
                 result.notImplemented();
+                break;
         }
     }
 
-    public void copyFile(File fromFile, File toFile) {
-        try (FileInputStream in = new FileInputStream(fromFile);
-             FileOutputStream out = new FileOutputStream(toFile);
-             FileChannel inChannel = in.getChannel();
-             FileChannel outChannel = out.getChannel()) {
-            inChannel.transferTo(0, inChannel.size(), outChannel);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
     }
 }
 
@@ -229,33 +192,56 @@ class SetWallPaperTask extends AsyncTask<Pair<Bitmap, String>, Boolean, Boolean>
 
     @Override
     protected final Boolean doInBackground(Pair<Bitmap, String>... pairs) {
-        String mode = pairs[0].second;
-        Bitmap bitmap = pairs[0].first;
         WallpaperManager wallpaperManager = WallpaperManager.getInstance(mContext);
-
         try {
-            switch (mode) {
-                case "1": // default
-                    wallpaperManager.setBitmap(bitmap);
+            switch (pairs[0].second) {
+                case "1": { // generic set
+                    wallpaperManager.setBitmap(pairs[0].first);
                     break;
-                case "2": // lock
+                }
+                case "2": { // lock screen
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                        wallpaperManager.setBitmap(bitmap, null, true, WallpaperManager.FLAG_LOCK);
+                        wallpaperManager.setBitmap(pairs[0].first, null, true, WallpaperManager.FLAG_LOCK);
+                    } else {
+                        wallpaperManager.setBitmap(pairs[0].first);
                     }
                     break;
-                case "3": // home
+                }
+                case "3": { // home screen
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                        wallpaperManager.setBitmap(bitmap, null, true, WallpaperManager.FLAG_SYSTEM);
+                        String manufacturer = Build.MANUFACTURER.toLowerCase();
+                        if (manufacturer.contains("oppo") || manufacturer.contains("xiaomi") || manufacturer.contains("vivo")) {
+                            // Fallback: mở chooser thay vì set trực tiếp để tránh reset launcher
+                            Uri tempUri = getImageUri(mContext, pairs[0].first);
+                            Intent intent = new Intent(Intent.ACTION_ATTACH_DATA);
+                            intent.setDataAndType(tempUri, "image/*");
+                            intent.putExtra("mimeType", "image/*");
+                            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            mContext.startActivity(Intent.createChooser(intent, "Set as Home Wallpaper"));
+                        } else {
+                            wallpaperManager.setBitmap(pairs[0].first, null, true, WallpaperManager.FLAG_SYSTEM);
+                        }
+                    } else {
+                        wallpaperManager.setBitmap(pairs[0].first);
                     }
                     break;
-                case "4": // both
+                }
+                case "4": { // both
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                        wallpaperManager.setBitmap(bitmap, null, true,
-                                WallpaperManager.FLAG_LOCK | WallpaperManager.FLAG_SYSTEM);
+                        wallpaperManager.setBitmap(
+                                pairs[0].first,
+                                null,
+                                true,
+                                WallpaperManager.FLAG_LOCK | WallpaperManager.FLAG_SYSTEM
+                        );
+                    } else {
+                        wallpaperManager.setBitmap(pairs[0].first);
                     }
                     break;
+                }
             }
-        } catch (IOException ex) {
+        } catch (Exception ex) {
             ex.printStackTrace();
             return false;
         }
@@ -265,5 +251,13 @@ class SetWallPaperTask extends AsyncTask<Pair<Bitmap, String>, Boolean, Boolean>
     @Override
     protected void onPostExecute(Boolean aBoolean) {
         AsyncWallpaperPlugin.res.success(aBoolean);
+    }
+
+    // Helper: convert bitmap to URI
+    private Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = android.provider.MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
     }
 }
