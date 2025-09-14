6b19a0ba1404d64ea7f8e6e1f7fd18b68c138c98
thanhnguyen <thanh.nguyen@go2joy.vn>
2025-09-14 11:28:26 +0700

    Update

--- android/src/main/java/com/codenameakshay/async_wallpaper/AsyncWallpaperPlugin.java
+++ android/src/main/java/com/codenameakshay/async_wallpaper/AsyncWallpaperPlugin.java
@@ -1,163 +1,122 @@
-package com.go2joy.async_wallpaper;
+package com.codenameakshay.async_wallpaper;
 
+import android.app.Activity;
 import android.app.WallpaperManager;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Build;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.core.content.FileProvider;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Locale;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import android.util.Pair;
 
-/** AsyncWallpaperPlugin */
-public class AsyncWallpaperPlugin implements FlutterPlugin, MethodCallHandler {
-  private MethodChannel channel;
-  static Result res;
-  private Context context;
+public class AsyncWallpaperPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
+    private static final String CHANNEL = "async_wallpaper";
+    private MethodChannel channel;
+    private Activity activity;
 
-  @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "async_wallpaper");
-    channel.setMethodCallHandler(this);
-    context = flutterPluginBinding.getApplicationContext();
-  }
-
-  @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    res = result;
-    if (call.method.equals("setWallpaper")) {
-      byte[] imageData = call.argument("imageData");
-      String wallpaperType = call.argument("wallpaperType");
-
-      if (imageData != null && wallpaperType != null) {
-        Bitmap bitmap = Utils.bytesToBitmap(imageData);
-        new SetWallPaperTask(context).execute(new Pair<>(bitmap, wallpaperType));
-      } else {
-        result.error("INVALID_ARGUMENTS", "Image data or wallpaper type missing", null);
-      }
-    } else {
-      result.notImplemented();
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), CHANNEL);
+        channel.setMethodCallHandler(this);
     }
-  }
 
-  @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    channel.setMethodCallHandler(null);
-  }
-}
-
-/**
- * Task async set wallpaper
- */
-class SetWallPaperTask extends AsyncTask<Pair<Bitmap, String>, Boolean, Boolean> {
-
-  private final Context mContext;
-
-  public SetWallPaperTask(final Context context) {
-    mContext = context;
-  }
-
-  @Override
-  protected final Boolean doInBackground(Pair<Bitmap, String>... pairs) {
-    WallpaperManager wallpaperManager = WallpaperManager.getInstance(mContext);
-    String manufacturer = Build.MANUFACTURER != null ? Build.MANUFACTURER.toLowerCase() : "";
-
-    try {
-      switch (pairs[0].second) {
-        case "1": { // default
-          if (needChooser(manufacturer)) {
-            openChooser(pairs[0].first, "Apply as:");
-          } else {
-            wallpaperManager.setBitmap(pairs[0].first);
-          }
-          break;
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        if (call.method.equals("setWallpaper")) {
+            String filePath = call.argument("filePath");
+            int wallpaperLocation = call.argument("wallpaperLocation"); // HOME / LOCK / BOTH
+            setWallpaper(filePath, wallpaperLocation, result);
+        } else {
+            result.notImplemented();
         }
-        case "2": { // lock only
-          if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            wallpaperManager.setBitmap(pairs[0].first, null, true, WallpaperManager.FLAG_LOCK);
-          } else {
-            wallpaperManager.setBitmap(pairs[0].first);
-          }
-          break;
-        }
-        case "3": { // home only
-          if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            if (needChooser(manufacturer)) {
-              openChooser(pairs[0].first, "Apply as Home Screen:");
-            } else {
-              wallpaperManager.setBitmap(pairs[0].first, null, true, WallpaperManager.FLAG_SYSTEM);
+    }
+
+    private void setWallpaper(String filePath, int wallpaperLocation, Result result) {
+        try {
+            File file = new File(filePath);
+            if (!file.exists()) {
+                result.error("FILE_NOT_FOUND", "Wallpaper file not found", null);
+                return;
             }
-          } else {
-            wallpaperManager.setBitmap(pairs[0].first);
-          }
-          break;
+
+            String manufacturer = Build.MANUFACTURER.toLowerCase(Locale.ROOT);
+            Log.d("AsyncWallpaper", "OEM = " + manufacturer);
+
+            // Dùng chooser cho Oppo, Realme, Vivo (tránh reset/đen màn hình)
+            if (manufacturer.contains("oppo") ||
+                manufacturer.contains("realme") ||
+                manufacturer.contains("vivo")) {
+
+                Uri uri = FileProvider.getUriForFile(activity,
+                        activity.getPackageName() + ".fileprovider", file);
+
+                Intent intent = new Intent(Intent.ACTION_SET_WALLPAPER);
+                intent.setDataAndType(uri, "image/*");
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+                activity.startActivity(Intent.createChooser(intent, "Set wallpaper using"));
+                result.success(true);
+                return;
+            }
+
+            // Các hãng khác: set trực tiếp
+            WallpaperManager wm = WallpaperManager.getInstance(activity);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                if (wallpaperLocation == 1) { // HOME
+                    wm.setBitmap(android.graphics.BitmapFactory.decodeFile(filePath), null, true,
+                            WallpaperManager.FLAG_SYSTEM);
+                } else if (wallpaperLocation == 2) { // LOCK
+                    wm.setBitmap(android.graphics.BitmapFactory.decodeFile(filePath), null, true,
+                            WallpaperManager.FLAG_LOCK);
+                } else { // BOTH
+                    wm.setBitmap(android.graphics.BitmapFactory.decodeFile(filePath));
+                }
+            } else {
+                wm.setBitmap(android.graphics.BitmapFactory.decodeFile(filePath));
+            }
+            result.success(true);
+
+        } catch (IOException e) {
+            result.error("SET_FAILED", "Failed to set wallpaper: " + e.getMessage(), null);
         }
-        case "4": { // both
-          if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            wallpaperManager.setBitmap(
-                pairs[0].first,
-                null,
-                true,
-                WallpaperManager.FLAG_LOCK | WallpaperManager.FLAG_SYSTEM
-            );
-          } else {
-            wallpaperManager.setBitmap(pairs[0].first);
-          }
-          break;
-        }
-      }
-    } catch (IOException ex) {
-      ex.printStackTrace();
-      return false;
     }
-    return true;
-  }
 
-  @Override
-  protected void onPostExecute(Boolean aBoolean) {
-    AsyncWallpaperPlugin.res.success(aBoolean);
-  }
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+    }
 
-  private boolean needChooser(String manufacturer) {
-    return manufacturer.contains("oppo") || manufacturer.contains("realme") || manufacturer.contains("vivo");
-  }
+    @Override
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+        activity = binding.getActivity();
+    }
 
-  private void openChooser(Bitmap bitmap, String title) throws IOException {
-    Uri uri = getImageUri(mContext, bitmap);
-    Intent setWall = new Intent(Intent.ACTION_ATTACH_DATA);
-    setWall.setDataAndType(uri, "image/*");
-    setWall.putExtra("mimeType", "image/*");
-    setWall.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-    setWall.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-    Intent chooser = Intent.createChooser(setWall, title);
-    chooser.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-    mContext.startActivity(chooser);
-  }
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
+        activity = null;
+    }
 
-  private Uri getImageUri(Context context, Bitmap bitmap) throws IOException {
-    File cachePath = new File(context.getCacheDir(), "images");
-    if (!cachePath.exists()) cachePath.mkdirs();
-    File file = new File(cachePath, "wallpaper.png");
-    FileOutputStream stream = new FileOutputStream(file);
-    bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
-    stream.close();
-    return FileProvider.getUriForFile(
-        context,
-        context.getPackageName() + ".fileprovider",
-        file
-    );
-  }
+    @Override
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+        activity = binding.getActivity();
+    }
+
+    @Override
+    public void onDetachedFromActivity() {
+        activity = null;
+    }
 }
